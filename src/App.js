0d0a338c78195d7b0dca9589c2e1f404e636648e
kimnewbie <gracecancho@gmail.com>
2021-08-01 23:35:02 +0900

    React with Ant Design API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Table } from 'antd';
+import "antd/dist/antd.css";
 import './App.css';
 
 function App() {
+
+  const [ loading, setLoading ] = useState(false)
+  const [ dataSource, setDataSource ] = useState([])
+  const [ page, setPage ] = useState(1)
+  const [ pageSize, setPageSize ] = useState(10)
+
+  useEffect( () => {
+
+    setLoading(true)
+    fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(response => response.json())
+    .then(data => {
+      setDataSource(data)
+    }).catch(err => {
+      console.log(err)
+    }).finally( () => {
+      setLoading(false)
+    })
+
+  }, [])
+  const columns = [
+    { 
+      key: "1",
+      title: 'ID',
+      dataIndex: 'id'
+    },
+    { 
+      key: "2",
+      title: 'User ID',
+      dataIndex: 'userId',
+      sorter: (record1, record2) => {
+        return record1.userId > record2.userId
+      }
+    },
+    { 
+      key: "3",
+      title: 'Status',
+      dataIndex: 'completed',
+      render: (completed) => {
+        return <p>{completed? 'Completed' : 'In Progress'}</p>
+      },
+      filters: [
+        {text:'Completed', value: true},
+        {text:'In Progress', value: false}
+      ],
+      onFilter: (value, record) => {
+        return record.completed === value
+      }
+    },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App"> 
+      <div className="App-header"> 
+        <Table
+          loading={loading}
+          columns={columns}
+          dataSource={dataSource}
+          pagination={{
+            current: page,
+            pageSize: pageSize,
+            total: 500,
+            onChange:(page,pageSize) => {
+              setPage(page);
+              setPageSize(pageSize)
+              //Make the api call here with page and page size
+            }
+          }}
         >
-          Learn React
-        </a>
-      </header>
+        </Table>
+      </div>  
     </div>
   );
 }
