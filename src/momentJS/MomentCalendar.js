3932fda45e7c2c4bd7b324158246867850b0d293
kimnewbie <gracecancho@gmail.com>
2021-09-05 20:35:16 +0900

    installed moment

--- src/momentJS/MomentCalendar.js
+++ src/momentJS/MomentCalendar.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React from "react";
+import moment from "moment";
 
 export default function MomentCalendar() {
+    const value = moment();
+    const startDay = value;
+
     return (
         <div>
 
